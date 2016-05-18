a241048912d4625732c2695f42ddb1d140072fda
Diogo <diogoblopes@gmail.com>
2016-05-18 14:10:03 +0200

    updated the bot to speak more naturaly

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id
         if (event.message && event.message.text) {
             text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            sendTextMessage(sender, "Text received! You said: " + text.substring(0, 200))
         }
     }
     res.sendStatus(200)
